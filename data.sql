c777366f0912eccf50bd2567876cae8ff8fcd3cd
Kamal Lama <kamal09rumba@gmail.com>
2018-11-18 21:11:50 +0545

    updated

--- data.sql
+++ data.sql
@@ -82,7 +82,7 @@ CREATE TABLE `auth_permission` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `content_type_id` (`content_type_id`,`codename`),
   CONSTRAINT `auth__content_type_id_1b778a2d39a4fe8e_fk_django_content_type_id` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,7 +91,7 @@ CREATE TABLE `auth_permission` (
 
 LOCK TABLES `auth_permission` WRITE;
 /*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
-INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can add permission',2,'add_permission'),(5,'Can change permission',2,'change_permission'),(6,'Can delete permission',2,'delete_permission'),(7,'Can add group',3,'add_group'),(8,'Can change group',3,'change_group'),(9,'Can delete group',3,'delete_group'),(10,'Can add user',4,'add_user'),(11,'Can change user',4,'change_user'),(12,'Can delete user',4,'delete_user'),(13,'Can add content type',5,'add_contenttype'),(14,'Can change content type',5,'change_contenttype'),(15,'Can delete content type',5,'delete_contenttype'),(16,'Can add session',6,'add_session'),(17,'Can change session',6,'change_session'),(18,'Can delete session',6,'delete_session'),(19,'Can add category',7,'add_category'),(20,'Can change category',7,'change_category'),(21,'Can delete category',7,'delete_category'),(22,'Can add product',8,'add_product'),(23,'Can change product',8,'change_product'),(24,'Can delete product',8,'delete_product');
+INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can add permission',2,'add_permission'),(5,'Can change permission',2,'change_permission'),(6,'Can delete permission',2,'delete_permission'),(7,'Can add group',3,'add_group'),(8,'Can change group',3,'change_group'),(9,'Can delete group',3,'delete_group'),(10,'Can add user',4,'add_user'),(11,'Can change user',4,'change_user'),(12,'Can delete user',4,'delete_user'),(13,'Can add content type',5,'add_contenttype'),(14,'Can change content type',5,'change_contenttype'),(15,'Can delete content type',5,'delete_contenttype'),(16,'Can add session',6,'add_session'),(17,'Can change session',6,'change_session'),(18,'Can delete session',6,'delete_session'),(19,'Can add category',7,'add_category'),(20,'Can change category',7,'change_category'),(21,'Can delete category',7,'delete_category'),(22,'Can add product',8,'add_product'),(23,'Can change product',8,'change_product'),(24,'Can delete product',8,'delete_product'),(25,'Can add order',9,'add_order'),(26,'Can change order',9,'change_order'),(27,'Can delete order',9,'delete_order'),(28,'Can add order item',10,'add_orderitem'),(29,'Can change order item',10,'change_orderitem'),(30,'Can delete order item',10,'delete_orderitem');
 /*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,7 +125,7 @@ CREATE TABLE `auth_user` (
 
 LOCK TABLES `auth_user` WRITE;
 /*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
-INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$20000$NqL52fTpD5c7$vbX3Vx82hC9E9PzEY8RY0kqmdHVP5wLztA+9e4LaSns=','2018-11-17 16:08:06.627814',1,'kamal','','','example@gmail.com',1,1,'2018-11-13 10:41:05.965710');
+INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$20000$NqL52fTpD5c7$vbX3Vx82hC9E9PzEY8RY0kqmdHVP5wLztA+9e4LaSns=','2018-11-18 13:44:06.213850',1,'kamal','','','example@gmail.com',1,1,'2018-11-13 10:41:05.965710');
 /*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -232,7 +232,7 @@ CREATE TABLE `django_content_type` (
   `model` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `django_content_type_app_label_77897a95c6dc90e2_uniq` (`app_label`,`model`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -241,7 +241,7 @@ CREATE TABLE `django_content_type` (
 
 LOCK TABLES `django_content_type` WRITE;
 /*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
-INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(3,'auth','group'),(2,'auth','permission'),(4,'auth','user'),(5,'contenttypes','contenttype'),(6,'sessions','session'),(7,'shop','category'),(8,'shop','product');
+INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(3,'auth','group'),(2,'auth','permission'),(4,'auth','user'),(5,'contenttypes','contenttype'),(9,'orders','order'),(10,'orders','orderitem'),(6,'sessions','session'),(7,'shop','category'),(8,'shop','product');
 /*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -258,7 +258,7 @@ CREATE TABLE `django_migrations` (
   `name` varchar(255) NOT NULL,
   `applied` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -267,7 +267,7 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2018-11-13 10:18:58.772882'),(2,'auth','0001_initial','2018-11-13 10:19:00.872567'),(3,'admin','0001_initial','2018-11-13 10:19:01.319858'),(4,'contenttypes','0002_remove_content_type_name','2018-11-13 10:19:02.199752'),(5,'auth','0002_alter_permission_name_max_length','2018-11-13 10:19:02.517005'),(6,'auth','0003_alter_user_email_max_length','2018-11-13 10:19:02.945777'),(7,'auth','0004_alter_user_username_opts','2018-11-13 10:19:03.025984'),(8,'auth','0005_alter_user_last_login_null','2018-11-13 10:19:03.344228'),(9,'auth','0006_require_contenttypes_0002','2018-11-13 10:19:03.347652'),(10,'sessions','0001_initial','2018-11-13 10:19:03.661970'),(11,'shop','0001_initial','2018-11-13 10:19:04.918081');
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2018-11-13 10:18:58.772882'),(2,'auth','0001_initial','2018-11-13 10:19:00.872567'),(3,'admin','0001_initial','2018-11-13 10:19:01.319858'),(4,'contenttypes','0002_remove_content_type_name','2018-11-13 10:19:02.199752'),(5,'auth','0002_alter_permission_name_max_length','2018-11-13 10:19:02.517005'),(6,'auth','0003_alter_user_email_max_length','2018-11-13 10:19:02.945777'),(7,'auth','0004_alter_user_username_opts','2018-11-13 10:19:03.025984'),(8,'auth','0005_alter_user_last_login_null','2018-11-13 10:19:03.344228'),(9,'auth','0006_require_contenttypes_0002','2018-11-13 10:19:03.347652'),(10,'sessions','0001_initial','2018-11-13 10:19:03.661970'),(11,'shop','0001_initial','2018-11-13 10:19:04.918081'),(12,'shop','0002_auto_20181118_1325','2018-11-18 13:25:31.895245'),(13,'orders','0001_initial','2018-11-18 13:25:33.019379');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -293,8 +293,71 @@ CREATE TABLE `django_session` (
 
 LOCK TABLES `django_session` WRITE;
 /*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
-INSERT INTO `django_session` VALUES ('5hql03sem4ce4f5gsnb31gtrclei55b6','YWZhYThhNjI5YzU1MDBmZTZlZjhlMjkzNjg5OGM4MmUxNDA3MDZhODp7Il9hdXRoX3VzZXJfaWQiOiIxIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoiZGphbmdvLmNvbnRyaWIuYXV0aC5iYWNrZW5kcy5Nb2RlbEJhY2tlbmQiLCJfYXV0aF91c2VyX2hhc2giOiIzYzYxYTQ2OGY5MDg1NjQyNDA3ZjdmZTM1YTY0MTAyNTk0NjBlYTVhIn0=','2018-11-27 10:41:30.629011'),('6lu1zyvl3ahps2rsrmd4pra19jtynzgs','ZWFkMjc5ZDFjZTA1YTE5N2JkY2M1MjY2NWEyYTEyYTU4ODJmZDc5ZDp7ImNhcnQiOnsiMTAiOnsicXVhbnRpdHkiOjksInByaWNlIjoiMTEzNC4wMCJ9fX0=','2018-12-02 12:34:15.689777');
+INSERT INTO `django_session` VALUES ('5hql03sem4ce4f5gsnb31gtrclei55b6','YWZhYThhNjI5YzU1MDBmZTZlZjhlMjkzNjg5OGM4MmUxNDA3MDZhODp7Il9hdXRoX3VzZXJfaWQiOiIxIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoiZGphbmdvLmNvbnRyaWIuYXV0aC5iYWNrZW5kcy5Nb2RlbEJhY2tlbmQiLCJfYXV0aF91c2VyX2hhc2giOiIzYzYxYTQ2OGY5MDg1NjQyNDA3ZjdmZTM1YTY0MTAyNTk0NjBlYTVhIn0=','2018-11-27 10:41:30.629011'),('sb2meglrcbx8g1po9kkx8pug9gfwqhii','ZTA0ZjU3YTViNTdhYWI3NGYyYTY3NzJiZTFkZjQxYmFhODRkNjVhODp7ImNhcnQiOnt9LCJfYXV0aF91c2VyX2lkIjoiMSIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIiwiX2F1dGhfdXNlcl9oYXNoIjoiM2M2MWE0NjhmOTA4NTY0MjQwN2Y3ZmUzNWE2NDEwMjU5NDYwZWE1YSJ9','2018-12-02 15:19:57.021685');
 /*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders_order`
+--
+
+DROP TABLE IF EXISTS `orders_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `address` varchar(250) NOT NULL,
+  `postal_code` varchar(20) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `paid` tinyint(1) NOT NULL,
+  `created` datetime(6) NOT NULL,
+  `updated` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders_order`
+--
+
+LOCK TABLES `orders_order` WRITE;
+/*!40000 ALTER TABLE `orders_order` DISABLE KEYS */;
+INSERT INTO `orders_order` VALUES (1,'abc','def','abc@abc.com','abc','1122','ktm',0,'2018-11-18 15:18:57.456918','2018-11-18 15:18:57.457004');
+/*!40000 ALTER TABLE `orders_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders_orderitem`
+--
+
+DROP TABLE IF EXISTS `orders_orderitem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders_orderitem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` int(10) unsigned NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orders_orderitem_order_id_5f7b5296a35448ff_fk_orders_order_id` (`order_id`),
+  KEY `orders_orderitem_product_id_48e1a4371ba85b00_fk_shop_product_id` (`product_id`),
+  CONSTRAINT `orders_orderitem_order_id_5f7b5296a35448ff_fk_orders_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders_order` (`id`),
+  CONSTRAINT `orders_orderitem_product_id_48e1a4371ba85b00_fk_shop_product_id` FOREIGN KEY (`product_id`) REFERENCES `shop_product` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders_orderitem`
+--
+
+LOCK TABLES `orders_orderitem` WRITE;
+/*!40000 ALTER TABLE `orders_orderitem` DISABLE KEYS */;
+INSERT INTO `orders_orderitem` VALUES (1,1134.00,1,1,10),(2,1500.00,1,1,5);
+/*!40000 ALTER TABLE `orders_orderitem` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -371,4 +434,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-18 18:29:24
+-- Dump completed on 2018-11-18 21:11:15
