a613b8beac6b08cecff940547c6418205a83f071
Kamal Lama <kamal09rumba@gmail.com>
2018-11-25 18:16:50 +0545

    weasyPrint imported

--- orders/views.py
+++ orders/views.py
@@ -45,17 +45,6 @@ def admin_order_detail(request,order_id):
 @staff_member_required
 def admin_order_pdf(request,order_id):
     order = get_object_or_404(Order, id=order_id)
-<<<<<<< HEAD
-    # html = render_to_string('orders/order/pdf.html', {'order': order})
-    # response = HttpResponse(content_type='application/pdf')
-    # response['Content-Disposition'] = 'filename= \
-    #                                   "order_{}.pdf"'.format(order.id)
-    # weasyprint.HTML(string=html).write_pdf(response,
-    #                                        stylesheets=[weasyprint.CSS(
-    #                                            settings.STATIC_ROOT+'css/pdf.css')])
-    # return response
-    return render(request, 'orders/order/pdf.html', {'order':order})
-=======
     html = render_to_string('orders/order/pdf.html', {'order': order})
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'filename= \
@@ -65,4 +54,3 @@ def admin_order_pdf(request,order_id):
                                                settings.STATIC_ROOT + '/css/pdf.css')])
     return response
     # return render(request, 'orders/order/pdf.html', {'order':order})
->>>>>>> pdf invoice template
