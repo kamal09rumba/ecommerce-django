ffab647a3d42243f259b841bb87c3d4a64b7d32a
Kamal Lama <kamal09rumba@gmail.com>
2018-11-25 17:16:51 +0545

    pdf invoice template

--- orders/admin.py
+++ orders/admin.py
@@ -11,6 +11,11 @@ def order_detail(obj):
     return '<a href="{}">View</a>'.format(reverse('orders:admin_order_detail',args=[obj.id]))
 order_detail.allow_tags = True
 
+def order_pdf(obj):
+    return '<a href="{}">pdf</a>'.format(reverse('orders:admin_order_pdf',args=[obj.id]))
+order_pdf.allow_tags = True
+order_pdf.short_description = 'PDF Bill'
+
 
 def export_to_csv(modeladmin,request,queryset):
     opts = modeladmin.model._meta
@@ -42,7 +47,8 @@ class OrderItemInline(admin.TabularInline):
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id','first_name','last_name','email','address','postal_code','city','paid','created','updated',order_detail]
+    list_display = ['id','first_name','last_name','email','address',
+                    'postal_code','city','paid','created','updated',order_detail,order_pdf]
     list_filter = ['paid','created','updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
